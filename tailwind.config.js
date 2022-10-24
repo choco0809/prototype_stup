055f5f2a666dbaea060078d7de2aa74340d660d5
choco <superbeaver.180809@gmail.com>
2022-10-24 19:54:17 +0900

    daisyuiをインストール

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,18 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  themes: [{
+    color: {
+      'base': '#FFF'
+    }
+  }],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [{
+      stup: {
+        'base': '#FFF',
+      }
+    }, "cupcake"]
+  }
 }
