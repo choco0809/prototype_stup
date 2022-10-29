e6e7c303d24070bd0caa369ce65c148edd8fd731
choco <superbeaver.180809@gmail.com>
2022-10-29 18:54:15 +0900

    カレンダーを修正

--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./calendar.vue";
 
 document.addEventListener("DOMContentLoaded", () => {
-  createApp(App).mount("#calendar")
+  const calendar = document.getElementById("calendar")
+  const userId = calendar.getAttribute("value")
+  createApp(App, {userId: userId}).mount("#calendar")
 });
