0b88608e318979d7d1223161ddf84e3ff8d99925
choco <superbeaver.180809@gmail.com>
2022-10-30 18:52:53 +0900

    Discord上から学習記録を開始、終了できる機能を実装した

--- app/models/discord_bot.rb
+++ app/models/discord_bot.rb
@@ -6,13 +6,41 @@ class DiscordBot
 
   def start
     @bot.command :s, description: '学習時間の記録を開始します。' do |event|
-      event.respond '学習時間の記録を開始しました。'
+      user = User.find_by(uid: event.user.id)
+      @records = StudyTimeRecord.where(user_id: user.id)
+      @start_time = Time.now
+      if @records.empty?
+        user.study_time_records.create(start_at: @start_time)
+        event.respond "学習を開始しました。#{@start_time}"
+      else
+        if @records.last.end_at.nil?
+          event.respond "前回の学習が終了していません。"
+        else
+          user.study_time_records.create(start_at: @start_time)
+          event.respond "学習を開始しました。#{@start_time}"
+        end
+      end
     end
 
     @bot.command :e, description: '学習時間の記録を終了します。' do |event|
-      event.respond '学習時間の記録を終了しました。'
+      user = User.find_by(uid: event.user.id)
+      @records = StudyTimeRecord.where(user_id: user.id)
+      @end_time = Time.now
+      if @records.last.end_at.nil?
+        user.study_time_records.update(end_at: @start_time)
+        event.respond "学習を終了しました。#{@end_time}"
+      else
+        event.respond '学習が開始されていません。'
+      end
     end
     @bot.run
+  end
+
+  private
+
+  # 最新の学習記録を取得する
+  def get_current_study_records
+
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   class << self
     def find_or_create_from_auth_hash(auth_hash)
       user_params = user_params_from_auth_hash(auth_hash)
-      find_or_create_by(email: user_params[:email]) do |user|
+      find_or_create_by(uid: user_params[:uid]) do |user|
         user.update(user_params)
       end
     end
@@ -15,6 +15,7 @@ class User < ApplicationRecord
         name: auth_hash.info.name,
         email: auth_hash.info.email,
         image: auth_hash.info.image,
+        uid: auth_hash.uid,
       }
     end
   end
