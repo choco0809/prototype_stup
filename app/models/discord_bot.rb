0f474ea8900fa0466b8d2b4da730a8d441f06f7a
choco <superbeaver.180809@gmail.com>
2022-10-30 19:33:27 +0900

    不具合修正

--- app/models/discord_bot.rb
+++ app/models/discord_bot.rb
@@ -7,11 +7,11 @@ class DiscordBot
   def start
     @bot.command :s, description: '学習時間の記録を開始します。' do |event|
       user = User.find_by(uid: event.user.id)
-      @records = StudyTimeRecord.where(user_id: user.id)
-      @start_time = Time.now.strftime("%Y-%m-%d %H:%M")
       if user.nil?
         event.respond "ユーザーが登録されていません。"
       else
+        @records = StudyTimeRecord.where(user_id: user.id)
+        @start_time = Time.now.strftime("%Y-%m-%d %H:%M")
         if @records.empty?
           user.study_time_records.create(start_at: @start_time)
           event.respond "学習を開始しました。#{@start_time}"
@@ -28,13 +28,17 @@ class DiscordBot
 
     @bot.command :e, description: '学習時間の記録を終了します。' do |event|
       user = User.find_by(uid: event.user.id)
-      @records = StudyTimeRecord.where(user_id: user.id)
-      @end_time = Time.now.strftime("%Y-%m-%d %H:%M")
-      if @records.last.end_at.nil?
-        user.study_time_records.update(end_at: @start_time)
-        event.respond "学習を終了しました。#{@end_time}"
+      if user.nil?
+        event.respond "ユーザーが登録されていません。"
       else
-        event.respond '学習が開始されていません。'
+        @records = StudyTimeRecord.where(user_id: user.id)
+        @end_time = Time.now.strftime("%Y-%m-%d %H:%M")
+        if @records.last.end_at.nil?
+          user.study_time_records.last.update(end_at: @start_time)
+          event.respond "学習を終了しました。#{@end_time}"
+        else
+          event.respond '学習が開始されていません。'
+        end
       end
     end
     @bot.run
